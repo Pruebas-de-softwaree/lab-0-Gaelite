4138e78e920ad577af97a204bb6335a67b4a780c
Gael García <129134162+Gaelite@users.noreply.github.com>
2025-08-22 03:03:35 +0000

    errores

--- operations.py
+++ operations.py
@@ -29,7 +29,23 @@ if __name__ == "__main__":
 
     print("start test")
 
+    print("La suma da 2 y 3 deberia ser 5, resultado: ", add(2, 3))
+    print("La suma da A y B deberia ser AB, resultado: ", add("A", "B"))
 
+    print("La resta de 2 y 3 deberia ser -1, resultado: ", subtract(2, 3))
+    print("La resta de -2 y -3 deberia ser 1, resultado:  ", subtract(-2, -3))
+
+    print("La multiplicacion de 2 y 3 deberia ser 6, resultado: ", multiply(2, 3))
+    print("La multiplicacion de 2 y -3 deberia ser -6, resultado: ", multiply(2, -3))
+
+    try:
+        print("La division de e y d deberia dar error", divide('e', 'd'))
+    except TypeError as e:
+        print("Error esperado (deberia devolver ed):", e)
+    try:
+        print("La division de 5 y 0 deberia dar undefined o dar error al usar sero como divisor", divide(5, 0))
+    except ZeroDivisionError as e:
+        print("Error esperado:", e)
 
     print("end test")
 
